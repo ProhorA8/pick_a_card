8405b975504c6b97aa3ff4e2820d61abbada8cd4
ProhorA8 <Procustom4@gmail.com>
2020-07-18 19:44:58 +0300

    Unicode suit symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,4 @@
 values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
+suits = ["♦", "♠", "♥ ", "♣"]
 
 puts "#{values.sample} of #{suits.sample}"
